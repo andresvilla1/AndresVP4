e590fbeffcac816adddbb8a63748712b7aa6773b
andresvilla1 <37849309+andresvilla1@users.noreply.github.com>
2019-02-15 15:17:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+	
+// "el" stands for the "element" that was just clicked
+function allOpaque(el){
+	el.classList.add("opaque");
+}
+
+// "el" stands for the "element" that was just clicked
+function removeColor(el){
+}
+
+function showIngram(el){
+}
